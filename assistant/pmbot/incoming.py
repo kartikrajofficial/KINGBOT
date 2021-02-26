0850d220a9e39e64c6f29cd3e7aac2770d399878
HITMAN | KARTIK <73695869+kartikrajofficial@users.noreply.github.com>
2021-02-26 14:55:21 +0530

    Update incoming.py

--- assistant/pmbot/incoming.py
+++ assistant/pmbot/incoming.py
@@ -1,9 +1,9 @@
-# Ultroid - UserBot
+# KINGBOT - UserBot
 # Copyright (C) 2020 TeamUltroid
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# 
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# .
 
 # https://github.com/xditya/TeleBot/blob/master/telebot/plugins/mybot/pmbot/incoming.py
 """
