d969bc395ffe76087fbda48a5b27c2088654ebbf
HITMAN | KARTIK <73695869+kartikrajofficial@users.noreply.github.com>
2021-02-26 15:18:17 +0530

    Update __init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -1,20 +1,14 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
-from pyUltroid import *
-from pyUltroid.dB.database import Var
+#CREDITS KINGBOT
+from pyKINGBOT import *
+from pyKINGBOT.dB.database import Var
 from telethon import Button, custom
 
-OWNER_NAME = ultroid_bot.me.first_name
-OWNER_ID = ultroid_bot.me.id
+OWNER_NAME = KINGBOT_bot.me.first_name
+OWNER_ID = KINGBOT_bot.me.id
 
 
 async def setit(event, name, value):
     try:
         udB.set(name, value)
     except BaseException:
-        return await event.edit("`Something Went Wrong`")
+        return await event.edit("`Something W3nt Wrong!!`")
