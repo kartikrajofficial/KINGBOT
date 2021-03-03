ecdfdb07db0f47b5d33af44a0bedf61b89daa5d7
HITMAN | KARTIK <73695869+kartikrajofficial@users.noreply.github.com>
2021-03-03 10:40:18 +0530

    Create startup.sh

---
+++ resources/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# KINGBOT - UserBot
+echo "
+ 
+         _  _____ _   _  ____ ____   ___ _____ 
+| |/ |_ _| \ | |/ ___| __ ) / _ |_   _|
+| ' / | ||  \| | |  _|  _ \| | | || |  
+| . \ | || |\  | |_| | |_) | |_| || |  
+|_|\_|___|_| \_|\____|____/ \___/ |_|  
+                  
+ 
+      Visit @KINGBOTOFFICIAL for updates!!
+"
+ 
+python3 -m pyKINGBOT
+© 2021 GitHub, Inc.
